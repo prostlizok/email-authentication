9d91c7890ab1fd49a9255fe59a1d5c35ff072224
Liza Kosterieva <prostlizok@gmail.com>
2024-10-03 14:19:18 +0300

    Project structure was added.

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <h2>Home</h2>
+        <div>
+          <Button>Test</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
